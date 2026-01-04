745ac2e5e56559ca9552ec950e2d88e3d3098b8a
Prakash Loganathan <prakash.loganathaan@gmail.com>
2026-01-04 12:33:02 -0600

    Data Engineering Scripts

---
+++ data_engineering/gpg_decryption_udtf.sql
@@ -0,0 +1,57 @@
+/*
+Script: gpg_decryption_udtf.sql
+Description: DDL statements to create the GPG decryption UDTF.
+Team: Data Engineering
+Date: 2026-01-04
+Parameters: csch - common Schema identifier (e.g., COMMON) | tsch - Target Schema identifier (e.g., DE))
+*/
+CREATE FUNCTION IF NOT EXISTS HACKATHON_DB.HACKATHON_{{tsch}}_SCH.GPG_DECRYPTION_UDTF(file_path string)
+returns table(line string)
+language python
+runtime_version = '3.11'
+external_access_integrations=(GENERAL_EAI)
+secrets = ('pvt_key'=HACKATHON_DB.HACKATHON_{{csch}}_SCH.PGP_PRIVATE_KEY
+           ,'pphrase'=HACKATHON_DB.HACKATHON_{{csch}}_SCH.PGP_PASSPHRASE)
+packages=('snowflake-snowpark-python', 'python-gnupg')
+handler='gpgdecryptor'
+log_level=info
+comment='hactathon GPG Decryption UDTF'
+as
+$$
+import _snowflake
+import gnupg
+import io
+import os
+import logging
+from snowflake.snowpark.files import SnowflakeFile
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('GPG Decryption')
+
+class gpgdecryptor:
+    def __init__(self):
+        private_key = _snowflake.get_generic_secret_string('pvt_key')
+        os.makedirs('/tmp/gnupg', exist_ok=True)
+        self.gpg = gnupg.GPG(gnupghome='/tmp/gnupg')
+        import_result = self.gpg.import_keys(private_key)
+        if not import_result.count:
+            raise ValueError("Failed to import GPG Private Kay")
+        logger.info("GPG Private Key Imported Successfully")
+
+    def process(self, file_path: str):
+        passphrase = _snowflake.get_generic_secret_string('pphrase')
+        logger.info(f"Decryption process starts...")
+
+        with SnowflakeFile.open(file_path, 'rb') as encrypted_file:
+            decrypted_data = self.gpg.decrypt_file(encrypted_file, passphrase=passphrase)
+
+            if not decrypted_data.ok:
+                logger.error(f"Decryption Failed: {decrypted_data.stderr}")
+                raise ValueError(f"Decryption Failed: {decrypted_data.stderr}")
+
+            decrypted_content = decrypted_data.data.decode('utf-8')
+            logger.info('Decryption Successfull. Yielding lines')
+            for line in io.StringIO(decrypted_content):
+                yield(line.strip(),)
+$$
+;
---
+++ data_engineering/split_string_udf.sql
@@ -0,0 +1,16 @@
+/*
+Script: gpg_decryption_udtf.sql
+Description: DDL statements to create the GPG decryption UDTF.
+Team: Data Engineering
+Date: 2026-01-04
+Parameters: csch - common Schema identifier (e.g., COMMON) | tsch - Target Schema identifier (e.g., DE))
+*/
+CREATE FUNCTION IF NOT EXISTS HACKATHON_DB.HACKATHON_{{tsch}}_SCH.SPLIT_STRING_UDF(line string, item_num integer)
+returns string
+comment = 'hactathon string split SPROC'
+log_level = info
+as
+$$
+split_part(line,',',item_num)
+$$
+;
